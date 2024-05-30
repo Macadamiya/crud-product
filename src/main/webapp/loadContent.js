277aa377ff5339982c664dec1f58c7f4c39ce66d
mj01p <107888270+mj01p@users.noreply.github.com>
2024-05-30 16:49:58 -0300

    creating html and css

--- src/main/webapp/loadContent.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('pages/principalpage.html')
-        .then(response => response.text())
-        .then(html => document.getElementById('content').innerHTML = html)
-        .catch(error => console.error('Erro ao carregar a página:', error));
-});
