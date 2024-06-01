91b7d8ee97f8d56ac38ec37873a7954a94cd7c54
Macadamiya <lyh1051274908@gmail.com>
2024-06-01 15:41:47 -0300

    feat(config): added cross domain cors settings

---
+++ src/main/java/com/crud_product/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.crud_product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200"); // 允许的前端域名
+        config.addAllowedHeader("*"); // 允许的请求头
+        config.addAllowedMethod("*"); // 允许的请求方法（GET, POST, PUT, DELETE, 等）
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
